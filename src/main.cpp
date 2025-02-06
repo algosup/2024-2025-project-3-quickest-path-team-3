cba7a628d9be515ebd3ee5bb30012253cc02360a
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2025-02-06 09:06:13 +0100

    Update main.cpp
    need to fix the save, load save file

--- src/main.cpp
+++ src/main.cpp
@@ -7,55 +7,75 @@
 #include <unordered_set>
 #include <boost/functional/hash.hpp>
 
-void saveGraph(const Graph& graph, const std::string& filename) {
-    std::ofstream outFile(filename, std::ios::out | std::ios::binary);
-    if (!outFile) {
-        std::cerr << "Failed to open file for writing.\n";
-        return;
-    }
-
-    const auto& adjList = graph.getAdjList();
-    for (size_t u = 0; u < adjList.size(); ++u) {
-        for (const auto& [v, weight] : adjList[u]) {
-            outFile << u << "," << v << "," << weight << "\n";
-        }
-    }
-
-    outFile.close();
-}
-
-void loadGraph(Graph& graph, const std::string& filename) {
-    std::ifstream inFile(filename, std::ios::in | std::ios::binary);
-    if (!inFile) {
-        std::cerr << "Failed to open file for reading.\n";
-        return;
-    }
-
-    std::string line;
-    while (std::getline(inFile, line)) {
-        std::stringstream ss(line);
-        int u, v, weight;
-
-        ss >> u;
-        ss.ignore(1, ','); // Ignore the comma
-        ss >> v;
-        ss.ignore(1, ','); // Ignore the comma
-        ss >> weight;
-
-        graph.addShortcut(u, v, weight);
-    }
-
-    inFile.close();
-}
+//void saveGraph(const Graph& graph, const std::string& filename) {
+//    std::ofstream outFile(filename, std::ios::out | std::ios::binary);
+//    if (!outFile) {
+//        std::cerr << "Failed to open file for writing.\n";
+//        return;
+//    }
+//
+//    const auto& adjList = graph.getAdjList();
+//    for (size_t u = 0; u < adjList.size(); ++u) {
+//        for (const auto& [v, weight] : adjList[u]) {
+//            outFile << u << "," << v << "," << weight << "\n";
+//        }
+//    }
+//
+//    outFile.close();
+//}
+//
+//void loadGraph(Graph& graph, const std::string& filename) {
+//    std::ifstream inFile(filename, std::ios::in | std::ios::binary);
+//    if (!inFile) {
+//        std::cerr << "Failed to open file for reading.\n";
+//        return;
+//    }
+//    graph.reserveNodes(24000000);
+//    // Load node index mapping
+//    size_t nodeIndexSize;
+//    inFile >> nodeIndexSize;
+//    inFile.ignore(); // Ignore the newline character
+//
+//    for (size_t i = 0; i < nodeIndexSize; ++i) {
+//        std::string line;
+//        std::getline(inFile, line);
+//        std::stringstream ss(line);
+//        std::string node;
+//        int index;
+//        std::getline(ss, node, ',');
+//        ss >> index;
+//        graph.nodeIndex[node] = index;
+//        graph.indexToNode.push_back(node);
+//    }
+//
+//    // Load adjacency list
+//    size_t adjListSize;
+//    inFile >> adjListSize;
+//    inFile.ignore(); // Ignore the newline character
+//
+//    graph.adjList.resize(adjListSize);
+//    for (size_t u = 0; u < adjListSize; ++u) {
+//        std::string line;
+//        while (std::getline(inFile, line)) {
+//            std::stringstream ss(line);
+//            int v, weight;
+//            char comma;
+//            ss >> u >> comma >> v >> comma >> weight;
+//            graph.adjList[u].emplace_back(v, weight);
+//        }
+//    }
+//
+//    inFile.close();
+//}
 
 int main() {
     Graph graph;
-    const std::string preprocessedFile = "data/preprocessed-graph.txt";
+    const std::string preprocessedFile = "data/preprocessed-graph.csv";
 
     std::ifstream preprocessedFileCheck(preprocessedFile, std::ios::in | std::ios::binary);
     if (preprocessedFileCheck) {
         // Preprocessed file exists, load the graph from the file
-        loadGraph(graph, preprocessedFile);
+        //loadGraph(graph, preprocessedFile);
     } else {
         // Preprocessed file does not exist, read the original graph data and preprocess it
         std::ifstream file("data/USA-roads.csv", std::ios::in | std::ios::binary);
@@ -82,32 +102,11 @@ int main() {
 
         file.close();
 
+        // Preprocess the graph
         graph.contractNodes();
 
-        // Add shortcuts directly to the graph
-        const auto& adjList = graph.getAdjList();
-        std::unordered_set<std::pair<int, int>, boost::hash<std::pair<int, int>>> existingEdges;
-
-        for (size_t u = 0; u < adjList.size(); ++u) {
-            for (const auto& [v, weightUV] : adjList[u]) {
-                existingEdges.emplace(u, v);
-            }
-        }
-
-        for (size_t u = 0; u < adjList.size(); ++u) {
-            for (const auto& [v, weightUV] : adjList[u]) {
-                for (const auto& [w, weightVW] : adjList[v]) {
-                    if (u != w && existingEdges.find({u, w}) == existingEdges.end()) {
-                        int shortcutWeight = weightUV + weightVW;
-                        graph.addShortcut(u, w, shortcutWeight);
-                        existingEdges.emplace(u, w);
-                    }
-                }
-            }
-        }
-
         // Save the preprocessed graph to a file
-        saveGraph(graph, preprocessedFile);
+       // saveGraph(graph, preprocessedFile);
     }
 
     RestApi api(graph);
